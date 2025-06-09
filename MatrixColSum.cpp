704c8173d063b78f27527b053961f4b83dac917e
sujithachalla97 <168841679+sujithachalla97@users.noreply.github.com>
2025-06-09 23:18:41 +0530

    Create MatrixColSum.cpp

---
+++ MatrixColSum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, c;
+    cin >> r >> c;
+
+    int mat[r][c];
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            cin >> mat[i][j];
+
+    for (int j = 0; j < c; j++) {
+        int sum = 0;
+        for (int i = 0; i < r; i++)
+            sum += mat[i][j];
+        cout << sum;
+    }
+
+    return 0;
+}
