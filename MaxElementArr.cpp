eaa0f1fb2f40e1a7582f21117ed41bd105bfd158
sujithachalla97 <168841679+sujithachalla97@users.noreply.github.com>
2025-06-09 23:21:40 +0530

    Create MaxElementArr.cpp

---
+++ MaxElementArr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int maxVal = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > maxVal)
+            maxVal = arr[i];
+
+    cout << maxVal;
+    return 0;
+}
